594784664fc5697ad219641e365c016e48b0909a
zw9love <18514075699@163.com>
2017-06-14 16:29:52 +0800

    增加瀑布流效果

--- src/pages/Home.component.ts
+++ src/pages/Home.component.ts
@@ -1,11 +1,12 @@
 import {Component, EventEmitter, Input, Output, ElementRef, ViewChild, AfterViewInit, OnInit} from '@angular/core';
 import Mock from 'mockjs'
+import $ from 'jquery'
 
 @Component({
   selector: 'Home',
   templateUrl: './Home.component.html',
   // template: '<hero-form></hero-form>',
-  styleUrls: ['../assets/css/style_home_common.css','../assets/css/style_recommend.css']
+  styleUrls: ['../assets/css/style_home_common.css']
 })
 
 export class Home {
@@ -13,14 +14,11 @@ export class Home {
     '推荐', '行业', '订阅', '时尚', '美妆', '教育', '推荐2', '行业2', '订阅2', '时尚2', '美妆2', '教育2',
     '推荐', '行业', '订阅', '时尚', '美妆', '教育', '推荐2', '行业2', '订阅2', '时尚2', '美妆2', '教育2'];
   activeIndex = 0;
-  mainData: object;
+  mainData = [] ;
+  lock = false
 
   constructor() {
 
-  }
-
-  firstClick(index) {
-    this.activeIndex = index
   }
 
   @ViewChild('smallCell')
@@ -28,29 +26,24 @@ export class Home {
   @ViewChild('bigCell')
   bigCellDiv: ElementRef;
 
-  // ngAfterViewInit这玩意就是事件
-  ngAfterViewInit() {
-    // 只能拿到一个
-    console.log(this.smallCellDiv)
-    console.log(this.bigCellDiv)
 
-    // this.smallCellDiv.nativeElement.style.backgroundColor = 'red';
-    // this.bigCellDiv.nativeElement.style.backgroundColor = 'red';
+  firstClick(index) {
+    this.activeIndex = index
   }
 
-  ngOnInit(){
+  // 模拟数据
+  getData(){
     /*
-      {
-       'title': '@ctitle(6,40)',
-       'author':'@cword(2,8)',
-       'msg_num|0-999':0,
-       'eye_num|0-999':0,
-       'isMovie':'@boolean',
-       },
-   */
-
-    this.mainData = Mock.mock({
-      'list|10': [{
+     {
+     'title': '@ctitle(6,40)',
+     'author':'@cword(2,8)',
+     'msg_num|0-999':0,
+     'eye_num|0-999':0,
+     'isMovie':'@boolean',
+     },
+     */
+    let data = Mock.mock({
+      'list|3': [{
         'list|4':[
           {
             'title': '@ctitle(6,100)',
@@ -63,8 +56,41 @@ export class Home {
       }],
     }).list
 
+    this.mainData = this.mainData.concat(data)
+
+    console.log(this.mainData)
     // 输出结果
     // console.log(JSON.stringify(this.mainData, null, 4))
+
+    this.lock = false
+
+  }
+
+  // 在Angular创建完组件的视图后调用。
+  ngAfterViewInit() {
+    // 只能拿到一个
+    console.log(this.smallCellDiv)
+    console.log(this.bigCellDiv)
+
+    // this.smallCellDiv.nativeElement.style.backgroundColor = 'red';
+    // this.bigCellDiv.nativeElement.style.backgroundColor = 'red';
+
+    $(window).scroll(()=>{
+      let sumHeight = $(document).height()
+      let clientHeight = $(window).height()
+      let scrollTop = $(window).scrollTop()
+      let n = (clientHeight + scrollTop) / sumHeight
+      if(n >= 0.9 && !this.lock){
+        this.lock = true
+        if(this.mainData.length > 20) return
+        this.getData()
+      }
+    })
+  }
+
+  // 当Angular初始化完成数据绑定的输入属性后，用来初始化指令或者组件。
+  ngOnInit(){
+    this.getData()
   }
 
 }
