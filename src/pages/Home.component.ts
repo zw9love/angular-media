f1a53b21cdda278d23327a52ed9f265fdf10693d
zw9love <415789992@qq.com>
2017-06-14 21:52:39 +0800

    修复body的bug

--- src/pages/Home.component.ts
+++ src/pages/Home.component.ts
@@ -14,16 +14,17 @@ export class Home {
     '推荐', '行业', '订阅', '时尚', '美妆', '教育', '推荐2', '行业2', '订阅2', '时尚2', '美妆2', '教育2',
     '推荐', '行业', '订阅', '时尚', '美妆', '教育', '推荐2', '行业2', '订阅2', '时尚2', '美妆2', '教育2'];
   asideData = [
-    {src:'../assets/img/myorder.png',name:'我的订阅'},
-    {src:'../assets/img/star.png',name:'我的收藏'},
-    {src:'../assets/img/fix_msg.png',name:'我的评论'},
-    {src:'../assets/img/suggestion.png',name:'意见反馈'},
-    {src:'../assets/img/quit.png',name:'退出'},
+    {src: '../assets/img/myorder.png', name: '我的订阅'},
+    {src: '../assets/img/star.png', name: '我的收藏'},
+    {src: '../assets/img/fix_msg.png', name: '我的评论'},
+    {src: '../assets/img/suggestion.png', name: '意见反馈'},
+    {src: '../assets/img/quit.png', name: '退出'},
   ]
   activeIndex = 0;
-  mainData = [] ;
+  mainData = [];
   lock = false
-  asideActive =  false
+  asideActive = false
+  timer = null
 
   constructor() {
 
@@ -34,26 +35,35 @@ export class Home {
   @ViewChild('bigCell')
   bigCellDiv: ElementRef;
 
-
+  // 一级菜单点击事件
   firstClick(index) {
     this.activeIndex = index
   }
 
-  showAside(){
+  // 显示侧边栏
+  showAside() {
+    if (this.asideActive) {
+      clearInterval(this.timer)
+      this.timer = setTimeout(() => {
+        $('body').removeClass('body')
+      }, 800)
+    } else {
+      $('body').addClass('body')
+    }
     this.asideActive = !this.asideActive
   }
 
   // 模拟数据
-  getData(){
+  getData() {
     let data = Mock.mock({
       'list|3': [{
-        'list|4':[
+        'list|4': [
           {
             'title': '@ctitle(6,100)',
-            'author':'@cword(2,8)',
-            'msg_num|0-999':0,
-            'eye_num|0-999':0,
-            'isMovie':'@boolean',
+            'author': '@cword(2,8)',
+            'msg_num|0-999': 0,
+            'eye_num|0-999': 0,
+            'isMovie': '@boolean',
           },
         ]
       }],
@@ -78,21 +88,21 @@ export class Home {
     // this.smallCellDiv.nativeElement.style.backgroundColor = 'red';
     // this.bigCellDiv.nativeElement.style.backgroundColor = 'red';
 
-    $(window).scroll(()=>{
+    $(window).scroll(() => {
       let sumHeight = $(document).height()
       let clientHeight = $(window).height()
       let scrollTop = $(window).scrollTop()
       let n = (clientHeight + scrollTop) / sumHeight
-      if(n >= 0.95 && !this.lock){
+      if (n >= 0.95 && !this.lock) {
         this.lock = true
-        if(this.mainData.length > 20) return
+        if (this.mainData.length > 20) return
         this.getData()
       }
     })
   }
 
   // 当Angular初始化完成数据绑定的输入属性后，用来初始化指令或者组件。
-  ngOnInit(){
+  ngOnInit() {
     this.getData()
   }
 
